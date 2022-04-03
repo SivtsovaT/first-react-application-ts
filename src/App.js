a0476418219797c7caf9602cabd79263073e9e7f
user <sivtsovatanya1985@gmail.com>
2022-04-03 13:47:34 +0300

    - added test paragraphs;

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+            <p>I can see there!</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <p>...and there!!!</p>
         </p>
       </div>
     );
